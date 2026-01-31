093fd5599e1f2dd45cbd6141e8fac11c9980c2b7
Sapana <sapananagar51@gmail.comgit config --global user.email sapananagar51@gmail.com>
2026-01-31 14:25:30 +0530

    Fix API filename and trailing slash redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 
     setLoading(true);
     try {
-      const res = await fetch('/api/pastes', {
+      const res = await fetch('/api/pastes/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ 
